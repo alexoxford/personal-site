b59ece69270524f209ba21b621820f18f6a737e9
Alex Oxford <alex@oakhaven.org>
2021-02-23 21:15:59 -0500

    Makes the vaccination chart responsive

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,6 +1,6 @@
 // install (please make sure versions match peerDependencies)
 // yarn add @nivo/core @nivo/line
-import { Line } from '@nivo/line';
+import { ResponsiveLine } from '@nivo/line';
 import React from 'react';
 // make sure parent container have a defined height when using
 // responsive component, otherwise height will be 0 and
@@ -10,33 +10,33 @@ import React from 'react';
 
 export function Graph({ data }: GraphProps) {
     return (
-        <Line
-            data={data}
-            margin={{ top: 100, right: 100, bottom: 100, left: 100 }}
-            width={1200}
-            height={600}
-            xScale={{
-                type: 'time',
-                format: '%Y-%m-%d',
-                useUTC: false,
-                precision: 'day',
-            }}
-            xFormat="time:%Y-%m-%d"
-            yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
-            yFormat=" >-.2%"
-            axisLeft={{ format: '>-.2%' }}
-            axisBottom={{ format: '%b %d' }}
-            curve='monotoneX'
-            enablePointLabel={false}
-            pointSize={8}
-            pointBorderWidth={1}
-            pointBorderColor={{
-                from: 'color',
-                modifiers: [['darker', 0.3]],
-            }}
-            useMesh={true}
-            enableSlices={false}
-        />);
+        <div style={{height: 400}}>
+            <ResponsiveLine
+                data={data}
+                margin={{ top: 10, right: 20, bottom: 50, left: 60 }}
+                xScale={{
+                    type: 'time',
+                    format: '%Y-%m-%d',
+                    useUTC: false,
+                    precision: 'day',
+                }}
+                xFormat="time:%Y-%m-%d"
+                yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+                yFormat=" >-.2%"
+                axisLeft={{ format: '>-.2%' }}
+                axisBottom={{ format: '%b %d' }}
+                curve='monotoneX'
+                enablePointLabel={false}
+                pointSize={8}
+                pointBorderWidth={1}
+                pointBorderColor={{
+                    from: 'color',
+                    modifiers: [['darker', 0.3]],
+                }}
+                useMesh={true}
+                enableSlices={false}
+            />
+        </div>);
 }
 
 export type GraphProps = {
