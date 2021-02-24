341d1309cee1ac504985032b463440de6108edcd
Alex Oxford <alex@oakhaven.org>
2021-02-23 20:50:27 -0500

    Improve graph appearance
    * Change bottom axis to default ticks (every 7 days)
    * Add left axis
    * Remove point labels
    * Make points smaller

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -10,40 +10,33 @@ import React from 'react';
 
 export function Graph({ data }: GraphProps) {
     return (
-    <Line
-        data={data}
-        margin={{ top: 100, right: 100, bottom: 100, left: 100 }}
-        width={1200}
-        height={600}
-        xScale={{
-            type: 'time',
-            format: '%Y-%m-%d',
-            useUTC: false,
-            precision: 'day',
-        }}
-        xFormat="time:%Y-%m-%d"
-        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
-        yFormat=" >-.2%"
-        axisLeft={null}
-        axisBottom={{
-            format: '%b %d',
-            tickValues: (data.flatMap((series) => {
-                return series.data.map((data) => {
-                    return new Date(data.x + 'T00:00:00');
-                });
-            }))
-        }}
-        curve='monotoneX'
-        enablePointLabel={true}
-        pointSize={16}
-        pointBorderWidth={1}
-        pointBorderColor={{
-            from: 'color',
-            modifiers: [['darker', 0.3]],
-        }}
-        useMesh={true}
-        enableSlices={false}
-    />);
+        <Line
+            data={data}
+            margin={{ top: 100, right: 100, bottom: 100, left: 100 }}
+            width={1200}
+            height={600}
+            xScale={{
+                type: 'time',
+                format: '%Y-%m-%d',
+                useUTC: false,
+                precision: 'day',
+            }}
+            xFormat="time:%Y-%m-%d"
+            yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+            yFormat=" >-.2%"
+            axisLeft={{ format: '>-.2%' }}
+            axisBottom={{ format: '%b %d' }}
+            curve='monotoneX'
+            enablePointLabel={false}
+            pointSize={8}
+            pointBorderWidth={1}
+            pointBorderColor={{
+                from: 'color',
+                modifiers: [['darker', 0.3]],
+            }}
+            useMesh={true}
+            enableSlices={false}
+        />);
 }
 
 export type GraphProps = {
