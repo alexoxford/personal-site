c363db0c35f12893b25e0e97dff42ad6e62d068d
Alex Oxford <alex@oakhaven.org>
2021-03-06 12:18:26 -0500

    Adds navbar with links to Twitter and projects

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import './index.css'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import VaccinationApp from './apps/vaccination/VaccinationApp'
 import HomepageApp from './apps/homepage/HomepageApp'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import twitterLogo from './images/Twitter-logo-blue.svg'
 
 ReactDOM.render(
   <Router>
+
+    <Navbar bg='dark' variant='dark' expand='lg'>
+      <Navbar.Brand>Alex's Crappy Site</Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+          <Nav.Link as={Link} to='/'>Home</Nav.Link>
+          <NavDropdown title='Projects' id='basic-nav-dropdown'>
+            <NavDropdown.Item as={Link} to='/vaccine'>Vaccination Progress Tracker</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav className='justify-content-end'>
+          <a href='https://www.twitter.com/AlexOxford3'>
+            <img src={twitterLogo} alt='Twitter' width='30' height='30' className='d-inline-block align-top' />
+          </a>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+
     <Switch>
       <Route path='/vaccine'>
         <VaccinationApp />
