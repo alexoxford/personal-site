21c5693037430e72768fda4f69b7760ebe6ed5af
Alex Oxford <alex@oakhaven.org>
2021-03-06 21:16:02 -0500

    Adds toggle for full vs. 80% adult population

--- src/apps/vaccination/VaccinationApp.tsx
+++ src/apps/vaccination/VaccinationApp.tsx
@@ -7,8 +7,10 @@ import React, { ReactElement } from 'react'
 
 Amplify.configure(awsconfig)
 
-const population = 8536000
-const dosesNeeded = 2 * population
+const virginiaPopulation = 8536000
+const dosesNeededMult = 2
+const fullyVaccinatedMult = 0.8
+const adultPopulationMult = 0.754
 
 interface DataPoint {
   date: string
@@ -36,19 +38,27 @@ const fetchData = async (): Promise<DataPoint[]> => {
     .then(result => result.data.listVaccineDataPoints?.items ?? new Array<DataPoint>())
 }
 
-class VaccinationApp extends React.Component<{}, { projection: boolean }> {
+class VaccinationApp extends React.Component<{}, { projection: boolean, fullPopulation: boolean }> {
   constructor (props: {}) {
     super(props)
-    this.state = { projection: true }
+    this.state = { projection: true, fullPopulation: true }
     this.handleProjectionToggleClick = this.handleProjectionToggleClick.bind(this)
+    this.handleFullPopulationToggleClick = this.handleFullPopulationToggleClick.bind(this)
   }
 
   handleProjectionToggleClick (): void {
     this.setState({ projection: !this.state.projection })
   }
 
+  handleFullPopulationToggleClick (): void {
+    this.setState({ fullPopulation: !this.state.fullPopulation })
+  }
+
   dataPointsToGraphDataPoints (points: DataPoint[]): GraphDataPoint[] {
     if (points.length < 2) { return new Array<GraphDataPoint>() }
+
+    const effectivePopulation = virginiaPopulation * (this.state.fullPopulation ? 1 : adultPopulationMult * fullyVaccinatedMult)
+    const dosesNeeded = effectivePopulation * dosesNeededMult
 
     var dataPoints: GraphDataPoint[] = points
       .sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
@@ -103,9 +113,19 @@ class VaccinationApp extends React.Component<{}, { projection: boolean }> {
 
                 {graph}
 
-                <button onClick={this.handleProjectionToggleClick}>Toggle Projection</button>
+                <div style={{
+                  maxWidth: '40%',
+                  minWidth: 'min-content',
+                  margin: '1rem auto',
+                  display: 'flex',
+                  justifyContent: 'space-between'
+                }}
+                >
+                  <button onClick={this.handleProjectionToggleClick}>Toggle Projection</button>
+                  <button onClick={this.handleFullPopulationToggleClick}>Toggle Full Population</button>
+                </div>
 
-                <p>The graph above shows the number of COVID-19 vaccine doses administered, as a percentage of twice the population of Virginia (accounting for the need for two doses of the vaccine).</p>
+                <p>100% = 2 doses * the population of Virginia{this.state.fullPopulation ? '' : ' * 0.8 (80% for herd immunity) * 0.754 (75.4% of Virginias are adults)'}</p>
               </div>
             )
           }}
