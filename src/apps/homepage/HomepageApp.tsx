ac4ca6ee8619ba4de8e96edfb738aa220006f5ff
Alex Oxford <alex@oakhaven.org>
2021-03-08 12:53:21 -0500

    Adds better placeholder content

--- src/apps/homepage/HomepageApp.tsx
+++ src/apps/homepage/HomepageApp.tsx
@@ -8,7 +8,14 @@ function HomepageApp (): ReactElement {
       <HomepageSidebar />
 
       <div className='homepage-content'>
-        <p>Placeholder Homepage</p>
+        <p>Hi, I'm Alex! I'm an engineer of the software and rocket variety, a "cook" (people who've eaten my... experiments may contest this label), queer, trans, and very mentally ill 😵
+          <br />
+          <br />
+
+          <span style={{ float: 'right' }}><i>&#8220;sex-obssessed and mildly terroristic&#8221;</i></span>
+          <br />
+          <span style={{ float: 'right' }}>&#8212;Anonymous</span>
+        </p>
       </div>
     </div>
   )
